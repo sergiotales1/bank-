48717031d6abac9191563880ab8e9b39d7345143
SergioTales1 <algumemailai3@gmail.com>
2023-10-18 22:22:50 -0300

    local storage implemented

--- js/script.js
+++ js/script.js
@@ -20,25 +20,33 @@ const account2 = {
 
 const accounts = [account1, account2];
 let currentAccount;
+
+// this is the function that store all accounts into localStorage
+function webStoreAccounts() {
+  // creating the test variables into local storage
+  accounts.forEach((element, index) => {
+    element.active = false;
+    localStorage.setItem(element.username, JSON.stringify(element));
+  });
+}
 // the login function only needs to redirect the user into the app page
 function login() {
+  webStoreAccounts();
   // saving the current account that we are into
   currentAccount = accounts.find(
     acc => acc.username === loginUsername.value && acc.pin === Number(loginPw.value)
   );
-  console.log(currentAccount);
+  currentAccount.active = true;
+  localStorage.setItem(currentAccount.username, JSON.stringify(currentAccount));
+
   // block to check if currentAccount is true (values corresponding)
   if (currentAccount) {
-    console.log('a');
     window.location.href = 'app.html';
-    console.log(currentAccount);
     // displayUI(currentAccount.movements); ------- ERROR we need a way to persist objects through page reload
   } else {
     // block in case of password or username inexistent
   }
 }
-
-console.log(currentAccount);
 
 // app.html
 // balance / movements
@@ -60,9 +68,22 @@ const closeUsername = document.getElementById('close-username-input');
 const closePin = document.getElementById('close-pin-input');
 const btnClose = document.querySelector('.close-btn');
 
-function displayUI(movements) {
+let movements;
+function displayUI() {
+  // this function will start when app.html load
+  // searching for the account with active true
+  accounts.forEach(acc => {
+    if (JSON.parse(localStorage.getItem(acc.username)).active === true) {
+      // storing that account into movements
+      movements = acc.movements;
+      return;
+    }
+  });
+
+  // using the active acc to display the ui
   labelBalance.innerHTML = movements.reduce((acc, cur) => acc + cur);
-  console.log(movements);
+
+  // looping through acc.movements and displaying the movements.
   movements.forEach((element, index) => {
     let type = element > 0 ? 'deposit' : 'withdrawal';
     let html = `
@@ -70,6 +91,7 @@ function displayUI(movements) {
         <div class="movement-value">${element}$</div>
   `;
 
-    movementsContainer.insertAdjacentHTML('afterend', html);
+    movementsContainer.insertAdjacentHTML('afterbegin', html);
   });
 }
+console.log(currentAccount);
